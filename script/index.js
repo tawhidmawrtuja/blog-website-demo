2229e7798dc373adb9894771d236f7d583345f98
tawhidmawrtuja <tawhidmawrtuja@hotmail.com>
2024-02-20 22:54:57 +0600

    blog-website

---
+++ script/index.js
@@ -0,0 +1,11 @@
+var menuIcon = document.getElementById("menu-icon");
+var menu = document.getElementById("menu");
+
+menuIcon.addEventListener("click", () => {
+
+    if(menu.className === "hidden"){
+        menu.classList.remove("hidden");
+    }else{
+        menu.classList.add("hidden")
+    }
+})
